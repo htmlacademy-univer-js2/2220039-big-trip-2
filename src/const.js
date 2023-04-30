72e778028b8b535111317865cd68b91954b64469
Akulinych <SemenovAA@urfu.me>
2023-04-30 15:47:13 +0500

    Вставил время, пока что работает

--- src/const.js
+++ src/const.js
@@ -1,1 +1,9 @@
 export const POINT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+
+export const DateFormat = {
+  MAIN: 'DD MMM', //число слева
+  VIEW: 'hh:mm', //время часы  12:25 — 13:35
+  EDIT: 'DD/MM/YY hh:mm', //19/03/19 00:00 в блоке редактирования точки
+  SERVICE: 'YYYY-MM-DDThh:mm', //в разметке в тэге date-time event__date datetime = "" > 11 jul</time>
+  SERVICE_MAIN: 'YYYY-MM-DD',
+};
